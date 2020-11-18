800f2c87a732b66c29131c41d99f40e79deca33e
gretelup <gretelup@gmail.com>
2020-11-17 20:12:11 -0500

    Develop sepal-length API route

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask, render_template, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 # Flask Setup
 app = Flask(__name__)
+
+# Database Setup using SQLAlchmy ORM
+engine = create_engine("sqlite:///iris.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Map table
+Iris = Base.classes.Iris
 
 @app.route('/')
 def home():
 
     return render_template("index.html")
+
+@app.route('/sepal-length')
+def sepal_length():
+    
+    # Query for all sepal-lengths
+    session = Session(engine)
+    results = session.query(Iris.Species, Iris.SepalLengthCm).all()
+
+    # Parse results
+    results_dict = {"Iris-virginica": [], "Iris-versicolor": [], "Iris-setosa": []}
+    for species, length in results:
+        results_dict[species].append(float(length))  
+    
+    return jsonify(results_dict)
 
 
 if __name__ == "__main__":
