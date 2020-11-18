69e8ddca55b03f051499adab646fb92e3f3e21db
gretelup <gretelup@gmail.com>
2020-11-17 22:47:30 -0500

    Format js

--- static/js/visuals.js
+++ static/js/visuals.js
@@ -1,129 +1,131 @@
 function optionChanged(selection) {
-    console.log(selection);
-    if (selection === "all") {
-        init();
-    } else {
-        d3.json("/sepal-length").then((response) => {
-            switch (selection) {
-                case "Iris-setosa":
-                    var trace = {
-                        x: response[selection],
-                        type: "histogram",
-                        opacity: 0.7,
-                        marker: {
-                            color: '#ffa6c1',
-                        },
-                        name: "Iris-setosa"
-                    };
-                    break;
-                case "Iris-versicolor":
-                    var trace = {
-                        x: response[selection],
-                        type: "histogram",
-                        opacity: 0.6,
-                        marker: {
-                            color: '#92daff',
-                        },
-                        name: "Iris-versicolor"
-                    };
-                    break;
-                case "Iris-virginica":
-                    var trace = {
-                        x: response[selection],
-                        type: "histogram",
-                        opacity: 0.5,
-                        marker: {
-                            color: '#46c45a',
-                        },
-                        name: "Iris-virginica"
-                    };
-                    break;
-            }
-
-            var data = [trace];
-
-            var layout = {
-                bargap: 0.01,
-                bargroupgap: 0.01,
-                barmode: "overlay",
-                title: "Sepal Length",
-                xaxis: {
-                    title: "Length in cm"
-                },
-                yaxis: {
-                    title: "Count"
-                }
-            };
-
-            var config = {
-                responsive: true
-            }
-
-            Plotly.newPlot('hist', data, layout, config);
-        });
-    }
-}
-
-function init() {
+  // If all is selected, render original histogram
+  if (selection === "all") {
+    init();
+  } else {
     d3.json("/sepal-length").then((response) => {
-
-        // Parse response object into arrays of sepal length for each species
-        var setosa = response["Iris-setosa"];
-        var versicolor = response["Iris-versicolor"];
-        var virginica = response["Iris-virginica"];
-
-        // Create trace for each species
-        var trace1 = {
-            x: setosa,
+      switch (selection) {
+        // Set trace to data for selected iris
+        case "Iris-setosa":
+          var trace = {
+            x: response[selection],
             type: "histogram",
             opacity: 0.7,
             marker: {
-                color: '#ffa6c1',
+              color: '#ffa6c1',
             },
             name: "Iris-setosa"
-        };
-
-        var trace2 = {
-            x: versicolor,
+          };
+          break;
+        case "Iris-versicolor":
+          var trace = {
+            x: response[selection],
             type: "histogram",
             opacity: 0.6,
             marker: {
-                color: '#92daff',
+              color: '#92daff',
             },
             name: "Iris-versicolor"
-        };
-
-        var trace3 = {
-            x: virginica,
+          };
+          break;
+        case "Iris-virginica":
+          var trace = {
+            x: response[selection],
             type: "histogram",
             opacity: 0.5,
             marker: {
-                color: '#46c45a',
+              color: '#46c45a',
             },
             name: "Iris-virginica"
-        };
+          };
+          break;
+      }
 
-        var data = [trace1, trace2, trace3];
+      var data = [trace];
 
-        var layout = {
-            bargap: 0.01,
-            bargroupgap: 0.01,
-            barmode: "overlay",
-            title: "Sepal Length",
-            xaxis: {
-                title: "Length in cm"
-            },
-            yaxis: {
-                title: "Count"
-            }
-        };
-
-        var config = {
-            responsive: true
+      var layout = {
+        bargap: 0.01,
+        bargroupgap: 0.01,
+        barmode: "overlay",
+        title: "Sepal Length",
+        xaxis: {
+          title: "Length in cm"
+        },
+        yaxis: {
+          title: "Count"
         }
+      };
 
-        Plotly.newPlot('hist', data, layout, config);
+      var config = {
+        responsive: true
+      }
+
+      Plotly.newPlot('hist', data, layout, config);
     });
+  }
+}
+
+function init() {
+  d3.json("/sepal-length").then((response) => {
+
+    // Parse response object into arrays of sepal length for each species
+    var setosa = response["Iris-setosa"];
+    var versicolor = response["Iris-versicolor"];
+    var virginica = response["Iris-virginica"];
+
+    // Create trace for each species
+    var trace1 = {
+      x: setosa,
+      type: "histogram",
+      opacity: 0.7,
+      marker: {
+        color: '#ffa6c1',
+      },
+      name: "Iris-setosa"
+    };
+
+    var trace2 = {
+      x: versicolor,
+      type: "histogram",
+      opacity: 0.6,
+      marker: {
+        color: '#92daff',
+      },
+      name: "Iris-versicolor"
+    };
+
+    var trace3 = {
+      x: virginica,
+      type: "histogram",
+      opacity: 0.5,
+      marker: {
+        color: '#46c45a',
+      },
+      name: "Iris-virginica"
+    };
+    // Create data for plot
+    var data = [trace1, trace2, trace3];
+
+    // Specify some formatting options 
+    var layout = {
+      bargap: 0.01,
+      bargroupgap: 0.01,
+      barmode: "overlay",
+      title: "Sepal Length",
+      xaxis: {
+        title: "Length in cm"
+      },
+      yaxis: {
+        title: "Count"
+      }
+    };
+
+    // Set chart to be responsive 
+    var config = {
+      responsive: true
+    }
+    Plotly.newPlot('hist', data, layout, config);
+  });
 }
 
 init();
